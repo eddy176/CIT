b0fab045f9eaaf707d7d485489c21a573f373256
kacystocks <kstocks0811@gmail.com>
2020-01-14 12:15:24 -0700

    Rename, consolidating files, added graphics and compilers

--- Programs/3600/projects/MazeRatProject/rat/Rat/graphics.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void DrawLine(double x1, double y1, double x2, double y2);
-void InitializeMyStuff();
-void DrawCircle(double x, double y, double r);
-void DrawTriangle(double x, double y, double height, double width, double length, double idk);
--- Programs/3600/projects/TextureDemo2012/TextureDemo2012/graphics.h
+++
@@ -1,5 +0,0 @@
-#include "glut.h"
-
-void DrawQuad(int x1, int y1, int x2, int y2, int i);
-
-enum viewtype {top_view, perspective_view, rat_view};
